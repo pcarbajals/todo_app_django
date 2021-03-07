fcba56427f37ad8184ed94616fc0f32401e282bc
Pablo Carbajal <pcarbajalsiller@kinaxis.com>
2021-03-07 18:05:48 -0500

    Added test cases for API

---
+++ api/todo/tests/api.py
@@ -0,0 +1,24 @@
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+
+from todo.models import Task
+
+
+class TaskAPITestCase(APITestCase):
+    ''' Test cases for the /api/v1/tasks endpoint '''
+
+    def test_get_tasks(self):
+        # TODO implement
+        pass
+
+    def test_post_new_task(self):
+        # TODO implement
+        pass
+
+    def test_patch_task(self):
+        # TODO implement
+        pass
+
+    def test_delete_task(self):
+        # TODO implement
+        pass
--- api/todo/tests/models.py
+++ api/todo/tests/models.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from todo.models import Task
 
 class TaskTestCase(TestCase):
+    ''' Test cases for the Task model '''
+
     def test_model_str_return_title(self):
         t = Task.objects.create(title='Task with title')
         self.assertEqual(str(t), 'Task with title')
