6d8277c45d556e43634f409da97d284a307ccd51
Pablo Carbajal <pcarbajalsiller@kinaxis.com>
2021-03-06 20:25:04 -0500

    Adds the Task model

--- api/todo/admin.py
+++ api/todo/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'status', 'due_date')
+
+admin.site.register(Task, TodoAdmin)
--- api/todo/models.py
+++ api/todo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    class Status(models.TextChoices):
+        TO_DO = 'T'
+        IN_PROGRESS = 'I'
+        PENDING = 'P'
+        DONE = 'D'
+
+    title = models.CharField(max_length=120, blank=False)
+    description = models.TextField()
+    status = models.CharField(max_length=1, choices=Status.choices, default=Status.TO_DO)
+    due_date = models.DateField(null=True)
+    
+
+    def _str_(self):
+        return self.title
